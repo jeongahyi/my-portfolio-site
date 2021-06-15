883a0026466cd739bf74b1394b5fe78f8f525532
Jeongah Yi <jeongahyi@gmail.com>
2021-06-15 17:25:56 +0900

    Add gatsby plugin image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Landing Page">
       <p>This is the landing page.</p>
+      <StaticImage
+        alt="This is me"
+        src="../images/austria1.jpg"
+      />
     </Layout>
   )
 }
